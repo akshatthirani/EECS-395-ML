2394ef6dfdec1de5029866552df56792fa3ae3f7
Tigist Diriba <tgdiriba@gmail.com>
2015-03-01 15:09:18 -0600

    Added poly K

--- src/train_soft_svm.m
+++ src/train_soft_svm.m
@@ -25,16 +25,27 @@ if isempty(x)
    x = rand(N,1); 
 end
 
+%% RBF Kernel Matrix
+% H = zeros(N);
+% for i=1:N
+%    for j=i:N
+%       H(i,j) = exp(-(norm(D(2:end,i)-D(2:end,j)).^2)/(2*sigma));
+%       H(j,i) = H(i,j);
+%    end
+% end
+
+%% Polynomial Kernel Matrix
 H = zeros(N);
 for i=1:N
    for j=i:N
-      H(i,j) = exp(-(norm(D(2:end,i)-D(2:end,j)).^2)/(2*sigma));
+      H(i,j) = (1+D(2:end,i)'*D(2:end,j))^3;
       H(j,i) = H(i,j);
    end
 end
 
 % Kernelized RBF Gaussian
-k_soft_svm_gradient = @(z)([ 2.*sum(H*z(2:end) + z(1).*ones(N,1)-b); 2.*max(ones(N,1)-b.*(H*z(2:end)+z(1).*ones(N,1)),zeros(N,1)).*(H*b) + lambda.*(H+H')*z(2:end) ]);
+% k_soft_svm_gradient = @(z)([ 2.*sum(H*z(2:end) + z(1).*ones(N,1)-b); 2.*max(ones(N,1)-b.*(H*z(2:end)+z(1).*ones(N,1)),zeros(N,1)).*(H*b) + lambda.*(H+H')*z(2:end) ]);
+k_soft_svm_gradient = @(z)([ 2.*b'*max(ones(N,1)-diag(b)*(H*z(2:end) + z(1).*ones(N,1)),zeros(N,1)); -2*H*diag(b)*max(ones(N,1)-diag(b)*(H*z(2:end) + z(1).*ones(N,1)),zeros(N,1)) + lambda.*(H+H')*z(2:end) ]);
 % k_soft_svm_gradient = @(z)([ 2.*sum(b'*max(zeros(N,1),ones(N,1) - b.*(H*z(2:end) + z(1).*ones(N,1)))); H*(H*z(2:end) + z(1).*ones(N,1) - b) + lambda.*(H+H')*z(2:end) ]);
 
 % Define SVM cost function
